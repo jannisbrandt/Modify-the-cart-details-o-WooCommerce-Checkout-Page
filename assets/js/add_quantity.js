1c1de0670840e16be29fdf8f8daa0197d52d5ff1
Jannis Brandt <jannisbrandt@Janniss-MacBook-Pro.local>
2020-05-20 15:56:27 +0200

    add javascript file for updating checkout page with ajax

---
+++ assets/js/add_quantity.js
@@ -0,0 +1,14 @@
+jQuery(function( $ ) {
+    $( "form.checkout" ).on( "click", "input.qty", function( e ) {
+        var data = {
+            action: 'update_order_review',
+            security: wc_checkout_params.update_order_review_nonce,
+            post_data: $( 'form.checkout' ).serialize()
+        };
+
+        jQuery.post( add_quantity.ajax_url, data, function( response )
+        {
+            $( 'body' ).trigger( 'update_checkout' );
+        });
+    });
+});
